b9c705f32f57ecdeea915efce3e9761009318386
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-02-25 15:37:51 +0530

    operands commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable default-case */
 import React,{useReducer} from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 import './App.css';
 
-const ACTIONS={
+export const ACTIONS={
   ADD_DIGIT:'add-digit',
   CHOOSE_OPERATION:'choose-operation',
   CLEAR:'clear',
@@ -10,12 +13,24 @@ const ACTIONS={
 }
 function reducer(state,{type,payload}){
   switch(type){
-    
+    case ACTIONS.ADD_DIGIT:
+      return{
+        ...state,
+        currentOperand:`${state.currentOperand  || ""}${payload.digit}`
+      } 
+      case ACTIONS.CHOOSE_OPERATION:
+        return{
+          ...state,
+          currentOperand:`${state.currentOperand  || ""}${payload.operation}`
+        } 
   }
 }
 function App() {
 
-  const [{currentOperand,previousOperand,operation},dispatch]=useReducer(reducer,{})
+  const [{currentOperand,previousOperand,operation},dispatch]=useReducer(
+    reducer,
+    {}
+    )
   return(
  <div className='calculator-grid'>
 
@@ -26,21 +41,21 @@ function App() {
 
   <button className='span-two'>AC</button>
   <button>DEL</button>
-  <button>/</button>
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>*</button>
-  <button>4</button>
-  <button>5</button>
-  <button>6</button>
-  <button>+</button>
-  <button>7</button>
-  <button>8</button>
-  <button>9</button>
-  <button>-</button>
-  <button>.</button>
-  <button>0</button>
+  <OperationButton dispatch={dispatch} operation='÷'/>
+  <DigitButton dispatch={dispatch} digit='1'/>
+  <DigitButton dispatch={dispatch} digit='2'/>
+  <DigitButton dispatch={dispatch} digit='3'/>
+  <OperationButton dispatch={dispatch} operation='*'/>
+  <DigitButton dispatch={dispatch} digit='4'/>
+  <DigitButton dispatch={dispatch} digit='5'/>
+  <DigitButton dispatch={dispatch} digit='6'/>
+  <OperationButton dispatch={dispatch} operation='+'/>
+  <DigitButton dispatch={dispatch} digit='7'/>
+  <DigitButton dispatch={dispatch} digit='8'/>
+  <DigitButton dispatch={dispatch} digit='9'/>
+  <OperationButton dispatch={dispatch} operation='-'/>
+  <DigitButton dispatch={dispatch} digit='.'/> 
+  <DigitButton dispatch={dispatch} digit='0'/> 
   <button className='span-two'>=</button>
   
 
